3a2337e1fa9fe9891db4c04e818ca9d1ceb5ef2f
prithviraj patil <prithviraj.a.patil@capgemini.com>
2019-12-06 18:26:29 +0530

    Data Table

---
+++ LIGHT/force-app/main/default/lwc/lWC_DataTable/lWC_DataTable.js
@@ -0,0 +1,20 @@
+import { LightningElement,track } from 'lwc';
+import fetchDataHelper from './fetchDataHelper';
+
+const columns = [
+    { label: 'Label', fieldName: 'name' },
+    { label: 'Website', fieldName: 'website', type: 'url' },
+    { label: 'Phone', fieldName: 'phone', type: 'phone' },
+    { label: 'Balance', fieldName: 'amount', type: 'currency' },
+    { label: 'CloseAt', fieldName: 'closeAt', type: 'date' },
+];
+
+export default class LWC_DataTable extends LightningElement {
+    @track data = [];
+    @track columns = columns;
+
+    async connectedCallback() {
+        const data = await fetchDataHelper({ amountOfRecords: 100 });
+        this.data = data;
+    }
+}
